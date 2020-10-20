d31d2ebb7ed82d9b0238d4c293e6ebe5d9ed1b80
matt <matt@redintro.io>
2020-10-20 12:25:36 +0100

    Moved GraphQL config to seperate file

---
+++ src/main/java/io/redintro/hexgraph/shared/GraphQLConfig.java
@@ -0,0 +1,40 @@
+package io.redintro.hexgraph.shared;
+
+import graphql.ExceptionWhileDataFetching;
+import graphql.GraphQLError;
+import graphql.servlet.GraphQLErrorHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class GraphQLConfig {
+    @Bean
+    public GraphQLErrorHandler errorHandler() {
+        return new GraphQLErrorHandler() {
+            @Override
+            public List<GraphQLError> processErrors(List<GraphQLError> errors) {
+                List<GraphQLError> clientErrors = errors.stream()
+                        .filter(this::isClientError)
+                        .collect(Collectors.toList());
+
+                List<GraphQLError> serverErrors = errors.stream()
+                        .filter(e -> !isClientError(e))
+                        .map(GraphQLErrorAdapter::new)
+                        .collect(Collectors.toList());
+
+                List<GraphQLError> e = new ArrayList<>();
+                e.addAll(clientErrors);
+                e.addAll(serverErrors);
+                return e;
+            }
+
+            protected boolean isClientError(GraphQLError error) {
+                return !(error instanceof ExceptionWhileDataFetching || error instanceof Throwable);
+            }
+        };
+    }
+}
